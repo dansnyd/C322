47f1ad2ba42410a23bedb025088fe5484c04dec8
dansnyd <dansnyd@iu.edu>
2023-02-02 13:04:00 -0500

    first commit

--- src/c322/labs/lab4/partb/LowerCaseTest.java
+++ src/c322/labs/lab4/partb/LowerCaseTest.java
@@ -1,4 +1,26 @@
 package c322.labs.lab4.partb;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class LowerCaseTest {
+
+    public static void main(String[] args) throws IOException {
+        int i;
+        try {
+            InputStream in =
+                    new LowerCaseStream(
+                            new BufferedInputStream(
+                                    new FileInputStream("test.txt")));
+            while((i = in.read()) >= 0) {
+                System.out.print((char)i);
+            }
+            in.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
